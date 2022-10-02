ebb1f267d543c4751d79d3b876c62e02d699fe59
Nikita Danilov <danilov@gmail.com>
2022-10-02 14:16:08 -0600

    bench.sh: add U1T workload.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- bench.sh
+++ bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for n in 8 ;do
-    printf "%7s, %7s, %7s, %5s, %5s, %5s, %5s, %5s, %5s\n" "N" "R" "M" "POSIX" "GO" "RUST" "U" "U1K" "U10K"
-    for r in 2 4 8 50 100 500 1000 5000 10000 50000 100000 500000 1000000 5000000 10000000 ;do
+for n in 2 8 ;do
+    printf "%7s, %7s, %7s, %5s, %5s, %5s, %5s, %5s, %5s, %5s\n" "N" "R" "M" "POSIX" "GO" "RUST" "U" "U1K" "U10K" "U1T"
+    for r in 2 4 8 50 100 500 1000 5000 10000 50000 100000 500000 1000000 ;do
 	m=$((8000000 / $n / $r + 100))
 	P=$(pmain $n $r $m     1 0 || echo 100000.0)
 	G=$(gmain $n $r $m     0 0 || echo 100000.0)
@@ -10,6 +10,7 @@ for n in 8 ;do
 	U=$(rmain $n $r $m 0 16 || echo 100000.0)
 	U1k=$(rmain $n $r $m 1000 16 || echo 100000.0)
 	U10k=$(rmain $n $r $m 10000 16 || echo 100000.0)
-	printf "%7d, %7d, %7d, %5.3f, %5.3f, %5.3f, %5.3f, %5.3f, %5.3f\n" $n $r $m $P $G $R $U $U1k $U10k
+	U1T=$(rmain $n $r $m 0 1 || echo 100000.0)
+	printf "%7d, %7d, %7d, %5.3f, %5.3f, %5.3f, %5.3f, %5.3f, %5.3f, %5.3f\n" $n $r $m $P $G $R $U $U1k $U10k $U1T
     done
 done
