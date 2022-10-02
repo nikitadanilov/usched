fb025f6d327c77b1cbaf50df93af4ee4fbb34c2e
Nikita Danilov <danilov@gmail.com>
2022-10-01 22:46:29 -0700

    Add a python script to plot bench.sh results.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

---
+++ graph.py
@@ -0,0 +1,52 @@
+import sys
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+from collections import defaultdict
+
+def drill():
+    return defaultdict(drill)
+
+def processinput(files):
+    d = drill()
+    processone(sys.stdin, d)
+    draw(d)
+
+def processone(f, d):
+    data = csv.reader(f)
+    for row in data:
+        r = [x.strip() for x in row]
+        if r[0] == 'N':
+            header = r
+        else:
+            r = [float(x) for x in r]
+            dd = d[r[0]][r[1]][r[2]];
+            ops = r[0]*r[1]*r[2]
+            for i in range(3, len(r)):
+                if len(dd[header[i]]) == 0:
+                    dd[header[i]] = []
+                dd[header[i]].append(ops/r[i])
+
+def draw(d):
+    fig, ax = plt.subplots()
+    ax.set_xscale('log')
+    f = drill()
+    for n in d:
+        for r in d[n]:
+            for m in d[n][r]:
+                for k in d[n][r][m]:
+                    v = d[n][r][m][k]
+                    avg = sum(v)/len(v)
+                    f[k][n*r] = [avg - min(v), avg, max(v) - avg]
+    for k in f:
+        F = f[k]
+        plt.errorbar(F.keys(), [F[t][1] for t in F],
+                     yerr = [[F[t][0] for t in F],
+                             [F[t][2] for t in F]], label=k)
+    plt.legend()
+    plt.savefig('out.svg', format='svg')
+    plt.show()
+    
+if __name__ == '__main__':
+    processinput(sys.argv[1:])
