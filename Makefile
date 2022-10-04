d4ebf81fcdd1b7422d58d13eba97f59e6c05c1f9
Nikita Danilov <danilov@gmail.com>
2022-10-04 16:04:26 -0700

    Add c++ coroutines benchmark.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ all:
 	cc -O3 -Wall pmain.c -pthread -opmain
 	cc -fno-stack-protector -Wall usched.c rr.c rmain.c -pthread -ormain
 	cc -O3 -fno-stack-protector -g -Wall usched.c umain.c -oumain
+	gcc -O3 -fcoroutines -Wall -I/usr/local/include c++main.cpp -L/usr/local/lib -lcppcoro -lstdc++ -pthread -oc++main
 	go build gmain.go
 	cd cycle; cargo build -r
---
+++ c++main.cpp
@@ -0,0 +1,70 @@
+/* gcc -O -fcoroutines -Wall coro.cpp -lcppcoro -lstdc++ -pthread  */
+/* https://github.com/andreasbuhr/cppcoro.git */
+#include <cppcoro/shared_task.hpp>
+#include <cppcoro/single_consumer_event.hpp>
+#include <cppcoro/task.hpp>
+#include <cppcoro/sync_wait.hpp>
+#include <cppcoro/when_all.hpp>
+#include <cppcoro/static_thread_pool.hpp>
+#include <iostream>
+#include <chrono>
+
+static int n;
+static int r;
+static int m;
+static int d;
+
+static std::vector<cppcoro::shared_task<void>>  t;
+static cppcoro::single_consumer_event          *e;
+static cppcoro::static_thread_pool             *tp;
+
+static cppcoro::shared_task<void> pingpong(int idx)
+{
+	auto next = idx / n * n + (idx + 1) % n;
+	//co_await tp->schedule();
+	for (int i = 0; i < m; ++i) {
+		if (idx % n == i % n) {
+			// std::cout << i << " * " << idx << " send\n";
+			e[next].set();
+			// std::cout << i << " * " << idx << " wait\n";
+			co_await e[idx];
+		} else {
+			// std::cout << i << "   " << idx << " wait\n";
+			co_await e[idx];
+			// std::cout << i << "   " << idx << " send\n";
+			e[next].set();
+		}
+	}
+}
+
+static cppcoro::shared_task<int> work()
+{
+	co_await cppcoro::when_all(t);
+	co_return 0;
+}
+
+using namespace std::chrono;
+
+int main(int argc, char **argv)
+{
+	uint32_t p;
+
+	n = std::atoi(argv[1]); /* Cycle length. */
+	r = std::atoi(argv[2]); /* Number of cycles. */
+	m = std::atoi(argv[3]); /* Number of rounds. */
+	d = std::atoi(argv[4]); /* Additional stack depth. */
+	p = std::atoi(argv[5]); /* Number of processors. */
+	cppcoro::static_thread_pool pool{p};
+	tp = &pool;
+
+	e = new cppcoro::single_consumer_event[n * r];
+	for (int i = 0; i < n * r; ++i) {
+		t.push_back(pingpong(i));
+	}
+
+	auto t0 = system_clock::now();
+	cppcoro::sync_wait(work());
+	auto t1 = system_clock::now();
+	printf("%f\n", duration_cast<nanoseconds>(t1 - t0).count() / 1000000000.);
+	delete[] e;
+}
