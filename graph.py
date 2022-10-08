71ab64b9cc0d1a29c654459957e4c3167bc576c0
Nikita Danilov <danilov@gmail.com>
2022-10-08 12:41:16 -0700

    Add results in a tabular form.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- graph.py
+++ graph.py
@@ -11,7 +11,7 @@ def drill():
 def processinput(files):
     d = drill()
     processone(sys.stdin, d)
-    draw(d)
+    draw(d, files[0])
 
 def processone(f, d):
     data = csv.reader(f)
@@ -28,7 +28,7 @@ def processone(f, d):
                     dd[header[i]] = []
                 dd[header[i]].append(ops/r[i])
 
-def draw(d):
+def draw(d, out):
     fig, ax = plt.subplots()
     ax.set_xscale('log')
     f = drill()
@@ -39,13 +39,19 @@ def draw(d):
                     v = d[n][r][m][k]
                     avg = sum(v)/len(v)
                     f[k][n*r] = [max(0, avg - min(v)), avg, max(0, max(v) - avg)]
+    first = True
     for k in f:
         F = f[k]
+        if first:
+            print("| | {} |".format("|".join(["{:.0f}".format(t) for t in F])))
+            print("|---|{}|".format("|".join(["---" for t in F])))
+            first=False
         plt.errorbar(F.keys(), [F[t][1] for t in F],
                      yerr = [[F[t][0] for t in F],
                              [F[t][2] for t in F]], label=k)
+        print("| {} | {} |".format(k, "|".join(["{:.2f}".format(F[t][1]/1000000) for t in F])))
     plt.legend(loc='lower right')
-    plt.savefig(sys.stdout.buffer, format='svg')
+    plt.savefig(out, format='svg')
     plt.show()
     
 if __name__ == '__main__':
